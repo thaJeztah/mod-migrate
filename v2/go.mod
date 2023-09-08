c1cc0f22bac91b30b53b04304ff2653a1af0fd4c
Sebastiaan van Stijn <github@gone.nl>
2023-09-08 20:23:25 +0200

    v2: update plugin to be compattible with v2
    Signed-off-by: Sebastiaan van Stijn <github@gone.nl>

--- v2/go.mod
+++ v2/go.mod
@@ -4,10 +4,10 @@ go 1.21.0
 
 require (
 	github.com/sirupsen/logrus v1.9.3
-	github.com/thaJeztah/mod-migrate/plugin v1.0.1
+	github.com/thaJeztah/mod-migrate/plugin v1.0.2
 )
 
 require (
-	github.com/thaJeztah/mod-migrate v1.0.1 // indirect
+	github.com/thaJeztah/mod-migrate v1.0.2 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
--- v2/main.go
+++ v2/main.go
@@ -13,14 +13,7 @@ type Plugin interface {
 
 func main() {
 	p := plugin.NewPlugin("v1.0.0 plugin")
-	fmt.Println(p.Hello())
-
-	// FIXME(thaJeztah): we can't use a v1 plugin in v2 because the signature is different (v1 "pkg/foo" !== v2 "pkg/foo")
-	// # github.com/thaJeztah/mod-migrate/v2
-	//  ./main.go:18:18: cannot use p (variable of type *plugin.Plugin) as Plugin value in argument to PrintPluginName: *plugin.Plugin does not implement Plugin (wrong type for method Hello)
-	// 		have Hello() *"github.com/thaJeztah/mod-migrate/pkg/foo".Hello
-	// 		want Hello() *"github.com/thaJeztah/mod-migrate/v2/pkg/foo".Hello
-	// PrintPluginName(p)
+	PrintPluginName(p)
 }
 
 func PrintPluginName(p Plugin) {
